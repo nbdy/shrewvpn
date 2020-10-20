391396a2b25711e5adbc4f563636557fda276958
nbdy <pascal@eberlein.io>
2020-10-20 23:53:14 +0200

    added dependencies and linking/copying

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y cmake build-essential bison flex libedit-dev
+sudo apt install -y cmake build-essential bison flex libedit-dev libssl-dev
 
 if [ ! -d build ]; then mkdir build;
 else (cd build; make clean); fi
@@ -9,3 +9,6 @@ cd build
 cmake ..
 make -j $(nproc)
 sudo make install
+
+sudo ln -s /usr/local/lib/libss_* /lib/x86_64-linux-gnu/
+sudo cp /usr/local/etc/iked.conf.sample /usr/local/etc/iked.conf
